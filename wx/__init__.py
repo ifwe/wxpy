b8f5a20db7b1743b33766f703e33f6061834c18d
Kevin Watters <kevinwatters@gmail.com>
2008-04-15 20:51:07 -0400

    Lots of changes. A working submodule system, lots of new controls.

---
+++ wx/__init__.py
@@ -0,0 +1,129 @@
+import _wxcore as wx
+from _wxcore import *
+
+def PyEventBinder(evttype, n):
+    return (evttype, )
+    
+
+_old_callafter = wx.CallAfter
+def CallAfter(func, *a, **k):
+    return _old_callafter(lambda: func(*a, **k))
+
+_old_bind = wx.EvtHandler.Bind
+def Bind(self, event, cb, source = None, id = -1, id2 = -1):
+    print self, type(self)
+    return _old_bind(self, event, cb, source, id, id2)
+
+_old_Dialog = wx.Dialog.__init__
+def __init__(self, parent = None, id = -1, title = "", pos = wx.DefaultPosition,
+             size = wx.DefaultSize, style = wx.DEFAULT_DIALOG_STYLE, name = 'dialogBox'):
+    return _old_Dialog(self, parent, id, title, pos, size, style, name)
+
+wx.TopLevelWindow.__repr__ = lambda tlw: '<wx.%s "%s" at %x>' % (type(tlw).__name__, tlw.GetTitle(), id(tlw))
+
+
+class CallLater:
+    """
+    A convenience class for `wx.Timer`, that calls the given callable
+    object once after the given amount of milliseconds, passing any
+    positional or keyword args.  The return value of the callable is
+    availbale after it has been run with the `GetResult` method.
+
+    If you don't need to get the return value or restart the timer
+    then there is no need to hold a reference to this object.  It will
+    hold a reference to itself while the timer is running (the timer
+    has a reference to self.Notify) but the cycle will be broken when
+    the timer completes, automatically cleaning up the wx.CallLater
+    object.
+
+    :see: `wx.CallAfter`
+    """
+    def __init__(self, millis, callable, *args, **kwargs):
+        self.millis = millis
+        self.callable = callable
+        self.SetArgs(*args, **kwargs)
+        self.runCount = 0
+        self.running = False
+        self.hasRun = False
+        self.result = None
+        self.timer = None
+        self.Start()
+
+    def __del__(self):
+        self.Stop()
+
+
+    def Start(self, millis=None, *args, **kwargs):
+        """
+        (Re)start the timer
+        """
+        self.hasRun = False
+        if millis is not None:
+            self.millis = millis
+        if args or kwargs:
+            self.SetArgs(*args, **kwargs)
+        self.Stop()
+        self.timer = wx.PyTimer(self.Notify)
+        self.timer.Start(self.millis, wx.TIMER_ONE_SHOT)
+        self.running = True
+    Restart = Start
+
+
+    def Stop(self):
+        """
+        Stop and destroy the timer.
+        """
+        if self.timer is not None:
+            self.timer.Stop()
+            self.timer = None
+
+
+    def GetInterval(self):
+        if self.timer is not None:
+            return self.timer.GetInterval()
+        else:
+            return 0
+
+
+    def IsRunning(self):
+        return self.timer is not None and self.timer.IsRunning()
+
+
+    def SetArgs(self, *args, **kwargs):
+        """
+        (Re)set the args passed to the callable object.  This is
+        useful in conjunction with Restart if you want to schedule a
+        new call to the same callable object but with different
+        parameters.
+        """
+        self.args = args
+        self.kwargs = kwargs
+
+
+    def HasRun(self):
+        return self.hasRun
+
+    def GetResult(self):
+        return self.result
+
+    def Notify(self):
+        """
+        The timer has expired so call the callable.
+        """
+        if self.callable and getattr(self.callable, 'im_self', True):
+            self.runCount += 1
+            self.running = False
+            self.result = self.callable(*self.args, **self.kwargs)
+        self.hasRun = True
+        if not self.running:
+            # if it wasn't restarted, then cleanup
+            wx.CallAfter(self.Stop)
+
+    Interval = property(GetInterval)
+    Result = property(GetResult)
+
+class PyDeadObjectError(Exception):
+    pass
+
+
+del wx
---
+++ wxpysetup.py
@@ -0,0 +1,97 @@
+'''
+
+utils for building wxpy or wxpy extensions
+
+'''
+
+from __future__ import with_statement
+
+import itertools
+import shutil
+import sipconfig
+import sipdistutils
+import sys
+import wxpyconfig
+import wxpyinterfaces
+import wxpyfeatures
+
+from distutils.core import Extension
+from path import path
+
+VERBOSE = True
+
+def different(file1, file2, start = 0):
+    if not file1.exists() or not file2.exists():
+        return True
+    
+    if file1.size != file2.size:
+        return True
+
+    return file1.bytes() != file2.bytes()
+
+def manage_cache(gendir):
+    """
+    This function keeps a cache of all sip-generated *.cpp and *.h files
+    and restores the stats of the newly generated set whenever the content
+    is unchanged
+    """
+    sipconfig.inform("Managing the module cache: %s" % gendir)
+    
+    gendir = path(gendir)
+    cache = gendir / 'cache'
+    cache.ensure_exists()
+    
+    if 'clean' in sys.argv:
+        cache.rmtree()
+    
+    for newfile in gendir.files('*.cpp') + gendir.files('*.h'):
+        oldfile = cache / newfile.name
+        if different(newfile, oldfile):
+            shutil.copy2(newfile, oldfile)
+            if VERBOSE:
+                sipconfig.inform("--> changed: %s" % newfile.name)
+        else:
+            shutil.copystat(oldfile, newfile)
+
+
+class wxpy_build_ext(sipdistutils.build_ext):
+    def generate_args_file(self, args):
+        argsfile = path(self.build_temp) / 'sipargs.txt'
+
+        argtext = []
+        for opt, arg in zip(args[::2], args[1::2]):
+            argtext.append(' '.join([opt, arg]))
+        argtext = '\n'.join(argtext)
+
+        if not argsfile.exists() or argsfile.text() != argtext:
+            argsfile.write_bytes(argtext)
+
+        return str(argsfile)
+
+    def _sip_compile(self, sip_bin, source, sbf):
+        features = wxpyfeatures.emit_features_file(wxpyinterfaces.SIP_DIR / 'features.sip')
+        feature_args = itertools.chain(*(('-x', feature) 
+            for feature, enabled in features.iteritems() if not enabled))
+
+        argsfile = self.generate_args_file([
+            "-c", self.build_temp, 
+            "-b", sbf,
+            '-t', wxpyconfig.sip_platform] + list(feature_args))
+
+        self.spawn([sip_bin, '-z', argsfile, source])
+
+    def swig_sources (self, sources, extension=None):
+        sources = sipdistutils.build_ext.swig_sources(self, sources, extension)
+
+        sipconfig.inform('build_temp is %s' % self.build_temp)
+        manage_cache(self.build_temp)
+        return sources
+
+def make_sip_ext(name, iface_files):
+    cxxflags = [f for f in wxpyconfig.cxxflags if not f.startswith('-O')]
+    #cxxflags.append('-O3')
+    
+    cargs = list(wxpyconfig.cxxflags) + ['-g']
+    largs = list(wxpyconfig.lflags)
+
+    return Extension(name, iface_files, extra_compile_args = cargs, extra_link_args = largs)
