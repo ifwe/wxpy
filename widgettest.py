5c08deb81ef2a753f85c73f9be0abab85cadc7db
unknown <Kevin@.rh.rit.edu>
2008-04-29 15:01:05 -0400

    More working compilation on MSW.

--- widgettest.py
+++ widgettest.py
@@ -1,12 +1,12 @@
 import sys
-import wx
-import wxextensions
-import sip
 import weakref
+
+import sip
+import wx
 
 def message(e):
     print 'click!', e.GetEventObject(), id(e.GetEventObject())
-    
+
 def p(*args):
     print ' '.join(str(a) for a in args)
 
@@ -17,45 +17,45 @@ class MyApp(wx.App):
         self.SetExitOnFrameDelete(False)
 
         wx.EntryStart()
-        
+
     def OnInit(self):
         print 'MyApp.OnInit'
         return True
 
     def MacOpenFile(self, filename):
         print 'Python MacOpenFile', filename
-    
+
     def MacPrintFile(self, filename):
         print 'Python MacPrintFile', filename
-        
+
     def MacNewFile(self):
         print 'Python MacNewFile'
-        
+
     def MacReopenApp(self):
         print 'Python MacReopenApp'
 
 def main():
       a = MyApp()
-      
+
 
       f = wx.Frame(None, -1, u'wxpy frame', (40, 40), (400, 300))
-      f.Bind(wx.EVT_ACTIVATE, lambda e: p('frame activate:', f.GetActive()))      
-      
+      f.Bind(wx.EVT_ACTIVATE, lambda e: p('frame activate:', f.GetActive()))
+
       s = wx.BoxSizer(wx.VERTICAL)
-      
+
       def show_modal(e):
           for x in xrange(1000):
               diag = wx.Dialog(f, -1, 'test modal dialog', size = (400, 300))
               diag.Destroy()
-              
+
           import gc
           print 'collect', gc.collect()
-      
+
       button = wx.Button(f, -1, 'Show Modal Dialog')
       button.Bind(wx.EVT_BUTTON, show_modal)
 
       print 'button id is', id(button)
-      
+
       def on_paint(e):
           dc = wx.PaintDC(f)
           dc.SetBrush(wx.Brush(wx.Colour(255, 0, 0)))
@@ -67,53 +67,52 @@ def main():
               font.SetWeight(wx.FONTWEIGHT_BOLD)
               dc.SetFont(font)
               dc.DrawText("hello, world!", 400, 300)
-              
+
               b2= b = wx.Brush(wx.Colour(255, 0, 0))
               #print 'id of brush', id(b)
               dc.SetBrush(b)
               b = dc.GetBrush()
-              
+
               dc.DrawRectangle(420, 500, 60, 30)
           else:
               print 'not ok!'
-              
+
       #f.Connect(-1, -1, 10090, on_paint)
       f.Bind(wx.EVT_PAINT, on_paint)
-      
-      static_text = wx.StaticText(f, -1, 'Static Text')      
+
+      static_text = wx.StaticText(f, -1, 'Static Text')
       checkbox = wx.CheckBox(f, -1, 'Checkbox')
-      
+
       def print_top(e): print wx.GetTopLevelWindows()
-          
+
       checkbox.Bind(wx.EVT_CHECKBOX, print_top)
 
       s.Add(static_text, 0, wx.ALL, 6)
       s.Add(button, 0, wx.ALL, 6)
       s.Add(checkbox, 0, wx.ALL, 6)
-      
+
       s.Add(wx.TextCtrl(f, -1, 'Text Control', name = 'Username'))
-      
+
       #for x in xrange(20):
       #    s.Add(wx.Button(f, -1, 'another button'))
 
       f.SetSizer(s)
       f.Layout()
-      
+
       a.Bind(wx.EVT_MENU, lambda e: a.ExitMainLoop(), id = wx.ID_EXIT)
 
 
       a.SetTopWindow(f)
       f.Show()
-      
-      # 
+
+      #
       # sys.a=a
-      
+
       print '--> calling MainLoop'
       import sip
       sip.dump(wx.GetApp())
       a.MainLoop()
       print '--> MainLoop returned'
-      
+
 if __name__ == '__main__':
     main()
-    
--- wxpyconfig.py
+++ wxpyconfig.py
@@ -15,44 +15,76 @@ if os.name == 'nt':
     platform_name = 'msw'
 elif 'Darwin' in platform.platform():
     platform_name = 'mac'
-    
+
 wxconfig = 'wx-config'
 
 if platform_name in ('mac',):
     #wxconfig = path('~/src/wxWebKitBranch-2.8/macbuild/wx-config').expand()
     wxconfig = path('~/wxpython-2.8/bin/wx-config').expand()
-    
+
     print 'using wxconfig:', str(wxconfig)
-    
+
     if not wxconfig.exists():
         raise AssertionError('cannot find wx-config at: "%s"' % wxconfig)
-        
-    
+
+
     cxxflags = shlex.split(wxconfig.run('--cxxflags').strip()) + ['-Wall', '-ggdb']
     lflags   = shlex.split(wxconfig.run('--libs %s' % ','.join(use_wx_libs)).strip())
-    
+
     # passed as -t argument
     sip_platform = 'WXMAC'
 
 elif platform_name == 'msw':
     wxdir = path(os.environ['WXDIR'])
     assert wxdir.exists()
-    
+
     print 'using wxwidgets dir:', wxdir
-    
-    # TODO: how to infer these without a wx-config binary?    
-    cxxflags = '/W4 /MD /DWIN32 /O2 /D__NO_VC_CRTDBG__ /D__WXMSW__ /D__WXDEBUG__ /D_UNICODE /DwxUSE_UNICODE_MSLU=1 /DwxUSE_GRAPHICS_CONTEXT=1'.split()
-    
+
+    DEBUG_SYMBOLS = True
+    ENABLE_EXCEPTIONS = True
+
+    # TODO: infer these without a wx-config binary? (bakefiles!)
+    cxxflags = ('/MD /DWIN32 /O2 /D__NO_VC_CRTDBG__ /D__WXMSW__ '
+                '/D__WXDEBUG__ /D_UNICODE /DwxUSE_UNICODE_MSLU=1 '
+                '/DwxUSE_GRAPHICS_CONTEXT=1 /DWXUSINGDLL').split()
+
+    if DEBUG_SYMBOLS: # debug
+        cxxflags.append('/Zi')
+
+    if ENABLE_EXCEPTIONS:
+        cxxflags.append('/EHa')
+
+
+    wx_lib_dir = wxdir / 'lib/vc_dll'
+    wx_flag = 'uh'
+    wx_config_dir = 'msw' + wx_flag
+
     # TODO: these too ;)
-    cxxflags.extend(['/I%s' % (wxdir / 'lib/vc_dll/mswuh'),
-                     '/I%s' % (wxdir / 'include')])
-    
-    lflags = '/LIBPATH:%s' % (wxdir / 'lib/vc_dll')
-    
+    cxxflags.extend(['/I%s' % str(wx_lib_dir / wx_config_dir),
+                     '/I%s' % str(wxdir / 'include')])
+
+    wx_libs = '''\
+base28%s
+base28%s_net
+base28%s_xml
+msw28%s_adv
+msw28%s_aui
+msw28%s_core
+msw28%s_html'''.split()
+
+    wx_libs = ['wx' + (s % wx_flag) + '.lib' for s in wx_libs]
+
+
+
+    lflags = ['/LIBPATH:' + str(wxdir / 'lib/vc_dll')]
+    lflags.extend(wx_libs)
+
+    if DEBUG_SYMBOLS:
+        lflags.append('/DEBUG')
+
     sip_platform = 'WXMSW'
-    
-    
-    
+
+
 assert 'sip_platform' in locals(), "Must set sip_platform"
 assert 'cxxflags' in locals(),     "Must set cxxflags"
 assert 'lflags' in locals(),       "Must set lflags"
