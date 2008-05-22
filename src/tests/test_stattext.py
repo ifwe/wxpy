18c00bd8a4cd709d76495df7e5ffb8e121ad9ac0
Kevin Watters <kevinwatters@gmail.com>
2008-05-22 18:55:30 -0400

    m_x and m_y properties for MouseEvent

--- src/tests/test_stattext.py
+++
@@ -1,7 +0,0 @@
-import wx
-from testutil import assert_ownership
-
-def test_StaticText():
-    f = wx.Frame(None)
-    assert_ownership(lambda: wx.StaticText(f, -1, 'test'), pyowned = False)
-    f.Destroy()
