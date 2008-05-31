e3aa435acd82549fcbf1d7f8fbf5f41fc4a4169e
Kevin Watters <kevinwatters@gmail.com>
2008-05-30 20:23:02 -0400

    webview's python facade

---
+++ wx/webview/__init__.py
@@ -0,0 +1,9 @@
+
+from _webview import *
+import wx
+
+_WebView = WebView
+class WebView(_WebView):
+    def __init__(self, parent, id = -1, point = wx.DefaultPosition, size = wx.DefaultSize,
+                 style = 0, name = 'WebView'):
+        _WebView.__init__(self, parent, id, point, size, style, name)
