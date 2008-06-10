c80b47cd62a98f0d4a71bf61d3c033d2bf9f4415
Kevin Watters <kevinwatters@gmail.com>
2008-06-09 20:24:18 -0400

    added a wrapper to test __nonzero__ on sip objects

---
+++ tests/test_wrapper.py
@@ -0,0 +1,30 @@
+import gc
+import sip
+import wx
+from weakref import ref
+
+def test_wrapper():
+    "test wrapped objects' __nonzero__"
+
+    f = wx.Frame(None)
+
+    assert not sip.isdeleted(f)
+    assert f
+
+    weak_f = ref(f)
+    f.Destroy()
+
+    # calling ProcessIdle here forces wxApp::DeletePendingObjects(), which
+    # deletes all objects from the wxPendingDelete list
+    wx.GetApp().ProcessIdle()
+
+    assert sip.isdeleted(f)
+    assert not f
+
+def main():
+    app = wx.PySimpleApp()
+    test_wrapper()
+
+
+if __name__ == '__main__':
+    main()
