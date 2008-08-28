10aa157e0779dff91db281e7526924389777ac42
Kevin Watters <kevinwatters@gmail.com>
2008-08-28 11:08:54 -0400

    Reentrant tests.

--- src/tests/__init__.py
+++ src/tests/__init__.py
@@ -7,7 +7,7 @@ def setup():
     '''
     Setup code run before all tests in this package.
     '''
-    assert not wx.GetApp() # make sure an app is created ONCE
+    #assert not wx.GetApp() # make sure an app is created ONCE
     sys.app = wx.PySimpleApp()
     assert sys.app == wx.GetApp()
     assert wx.GetApp() is not None
